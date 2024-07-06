fdc506e6b8b99d2aaecd7f8007187888640d7b70
Abiola Bolaji <ab2113@princeton.edu>
2024-07-06 15:56:34 -0400

    dsa

--- taint_tracking/riscv_crypto_fu_saes32_taint.tcl
+++ taint_tracking/riscv_crypto_fu_saes32_taint.tcl
@@ -7,6 +7,7 @@ reset rst
 
 # check if there is a timing attack possible that leaks secret
 assert {}
+#check the hierachy of the modules by looking at the outputs of the aes 
 
 # Set the time limit to 1 hour (3600 seconds)
 set_prove_time_limit 3600
