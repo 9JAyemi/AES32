cfd671cab93a9b9899c2b200639d5b23e3cb01a0
Abiola Bolaji <ab2113@princeton.edu>
2024-07-07 14:54:09 -0400

    fixed tcl

--- taint_tracking/riscv_crypto_fu_saes32_taint.tcl
+++ taint_tracking/riscv_crypto_fu_saes32_taint.tcl
@@ -2,8 +2,7 @@ analyze -sv riscv_crypto_fu_saes32_taint.v
 
 elaborate -top riscv_crypto_fu_saes32
 
-clock clk
-reset rst
+
 
 # check once the computation is finsihed that there is no information to the registar where data is stored
 assume {rs1_t == 1}
